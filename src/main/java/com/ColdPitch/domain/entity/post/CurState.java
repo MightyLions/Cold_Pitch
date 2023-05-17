098f959a4e2cebd2859922ad4b8bafa96b1d3067
dlrjs2360 <lhg3670@naver.com>
2023-05-18 00:33:45 +0900

    feat(post): 게시글 상태코드에 문자열로 상태값 입력 #19

--- src/main/java/com/ColdPitch/domain/entity/post/CurState.java
+++ src/main/java/com/ColdPitch/domain/entity/post/CurState.java
@@ -1,5 +1,14 @@
 package com.ColdPitch.domain.entity.post;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
 public enum CurState {
-    CLOSED, OPEN, PRIVATE
+    CLOSED("CLOSED"), OPEN("OPEN"), PRIVATE("PRIVATE");
+
+    private final String status;
+    CurState(String status) {
+        this.status = status;
+    }
 }
