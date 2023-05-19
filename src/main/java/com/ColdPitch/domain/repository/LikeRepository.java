16fdef539b3ad59e368733e3cc9ed37992ce3ef0
dlrjs2360 <lhg3670@naver.com>
2023-05-20 01:59:36 +0900

    feature(like): Like 도메인 객체, 리포지토리 생성

---
+++ src/main/java/com/ColdPitch/domain/repository/LikeRepository.java
@@ -0,0 +1,8 @@
+package com.ColdPitch.domain.repository;
+
+import com.ColdPitch.domain.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like,Long> {
+
+}
