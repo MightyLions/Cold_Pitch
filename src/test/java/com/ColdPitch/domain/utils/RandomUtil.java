d96706f159bea05411280dcbd62d270843a5c652
mon0mon <mon0mon@naver.com>
2023-05-21 00:38:50 +0900

    feat(solution): 솔루션 테스트 코드 추가 refactor(comment): 랜덤 값 생성 코드 분리

---
+++ src/test/java/com/ColdPitch/domain/utils/RandomUtil.java
@@ -0,0 +1,21 @@
+package com.ColdPitch.domain.utils;
+
+import java.util.Random;
+
+public class RandomUtil {
+    /**
+     * 랜덤 Long을 생성하는 메소드
+     *
+     * @param end inclusive
+     * @return 0부터 end 이하까지의 범위
+     */
+    public static long getRandom(int end) {
+        Random random = new Random();
+
+        return (long) random.nextInt(end);
+    }
+
+    public static double getRandomPercentage() {
+        return 1 - Math.random();
+    }
+}
