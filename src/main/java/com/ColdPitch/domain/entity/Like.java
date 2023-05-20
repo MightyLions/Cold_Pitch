b64d06e16a81aef1e87f17e1762b2c32bb02a2e9
mon0mon <mon0mon@naver.com>
2023-05-20 22:41:20 +0900

    fix(like, dislike): like 테이블이 예약어로 인한 오류 수정

--- src/main/java/com/ColdPitch/domain/entity/Like.java
+++ src/main/java/com/ColdPitch/domain/entity/Like.java
@@ -15,7 +15,7 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @SuperBuilder(toBuilder = true)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "like")
+@Table(name = "like_table")
 public class Like extends BaseEntity{
 
     @Id
