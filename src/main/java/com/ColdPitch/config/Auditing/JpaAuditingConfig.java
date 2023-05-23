8e45a6b7bafc5640352312425f4ea0a9964ece37
dlrjs2360 <lhg3670@naver.com>
2023-05-23 13:44:14 +0900

    fix(post): JPA Auditing 랜덤으로 생성되던 코드 수정
    - 수정인, 생성인 이메일로 저장

--- src/main/java/com/ColdPitch/config/Auditing/JpaAuditingConfig.java
+++ src/main/java/com/ColdPitch/config/Auditing/JpaAuditingConfig.java
@@ -1,15 +1,14 @@
-package com.ColdPitch.config;
+package com.ColdPitch.config.Auditing;
 
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-public class BaseConfig {
+public class JpaAuditingConfig {
     @Bean
     public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of(UUID.randomUUID().toString());
+        return new AuditorAwareImpl();
     }
 }
