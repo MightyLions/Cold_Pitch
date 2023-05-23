52dc2cf063f3d8428ca8100a5f238e35311d83b1
kdybe01 <kdybe01@gmail.com>
2023-05-23 10:17:48 +0900

    Feat: 사업자 등록번호 Controller 추가 - CompanyRegistrationController.java 추가

---
+++ src/main/java/com/ColdPitch/domain/apicontroller/CompanyRegistrationController.java
@@ -0,0 +1,33 @@
+package com.ColdPitch.domain.apicontroller;
+
+import com.ColdPitch.domain.entity.dto.companyRegistraion.CompanyRegistrationDto;
+import com.ColdPitch.domain.service.CompanyRegistrationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class CompanyRegistrationController {
+    private final CompanyRegistrationService companyRegistrationService;
+
+    @PostMapping("/validate")
+    public ResponseEntity<CompanyRegistrationDto> validate(@Valid @RequestBody CompanyRegistrationDto companyRegistrationDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        boolean isValid = companyRegistrationService.validateAndSaveCompanyRegistration(companyRegistrationDto);
+
+        if (!isValid) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(companyRegistrationDto);
+    }
+}
