ea93b398795e37cc1a2c6b6d7567da68d49677dc
kdybe01 <kdybe01@gmail.com>
2023-05-25 10:07:32 +0900

    Feature: 게시글 조회를 위한 findByUserId() 추가 - 싫어요를 선택한 게시글을 userId로 조회하기 위한 메서드 추가

--- src/main/java/com/ColdPitch/domain/repository/DislikeRepository.java
+++ src/main/java/com/ColdPitch/domain/repository/DislikeRepository.java
@@ -2,10 +2,14 @@ package com.ColdPitch.domain.repository;
 
 import com.ColdPitch.domain.entity.Dislike;
 import com.ColdPitch.domain.entity.Like;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DislikeRepository extends JpaRepository<Dislike,Long> {
 
     Optional<Dislike> findByUserIdAndPostId(Long id, Long postId);
+
+    Optional<List<Dislike>> findByUserId(Long userId);
 }
