2b3d06ee126926feb4f3f7fe49c33ecd9f7e650c
kdybe01 <kdybe01@gmail.com>
2023-05-25 16:59:25 +0900

    Refactor: 유저가 작성한 댓글 조회를 repository가 아닌 service 이용 - findByUserId() 삭제

--- src/main/java/com/ColdPitch/domain/repository/CommentRepository.java
+++ src/main/java/com/ColdPitch/domain/repository/CommentRepository.java
@@ -4,11 +4,6 @@ import com.ColdPitch.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
-
-    Optional<List<Comment>> findByUserId(Long userId);
 }
