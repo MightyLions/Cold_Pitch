434a916b6e541ef147a0595e35deaf8062550b6d
mon0mon <mon0mon@naver.com>
2023-05-26 01:25:07 +0900

    feat(comment): Swagger API 추가

--- src/main/java/com/ColdPitch/domain/entity/dto/comment/CommentResponseDto.java
+++ src/main/java/com/ColdPitch/domain/entity/dto/comment/CommentResponseDto.java
@@ -5,6 +5,9 @@ import com.ColdPitch.domain.entity.comment.CommentState;
 import com.ColdPitch.domain.entity.dto.post.PostResponseDto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.istack.NotNull;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,23 +16,30 @@ import lombok.Data;
 @Data
 @Builder
 @AllArgsConstructor
+@Schema(description = "댓글 응답 DTO")
 public class CommentResponseDto {
     @NotNull
+    @ApiModelProperty(name = "댓글 id")
     private Long id;
 
     @NotNull
+    @ApiModelProperty(name = "댓글을 작성한 유저 id")
     private Long userId;
 
     @NotNull
+    @ApiModelProperty(name = "댓글을 작성한 게시글 id")
     private Long postId;
 
     @NotNull
+    @ApiModelProperty(name = "댓글 내용")
     private String text;
 
     @JsonProperty("pCommentId")
+    @ApiModelProperty(name = "대댓글 id", allowEmptyValue = true)
     private Long pCommentId;
 
     @NotNull
+    @ApiModelProperty(name = "댓글 현재 상태(OPEN, DELETED)")
     private CommentState status;
 
     private LocalDateTime createAt;
