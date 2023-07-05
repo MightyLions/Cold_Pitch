42e8eeca0da2056035938e5149eaf75c0b1ed349
mon0mon <mon0mon@naver.com>
2023-07-05 22:43:08 +0900

    refactor(Validation) - Enum값 유효성 검증 추가 - ValidEum 어노테이션 추가

---
+++ src/main/java/com/ColdPitch/validation/EnumValidator.java
@@ -0,0 +1,34 @@
+/**
+ * @project ColdPitch
+ * @author ARA
+ * @since 2023-07-05 PM 9:57
+ */
+
+package com.ColdPitch.validation;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
+    private ValidEnum annotation;
+
+    @Override
+    public void initialize(ValidEnum constraintAnnotation) {
+        this.annotation = constraintAnnotation;
+    }
+
+    @Override
+    public boolean isValid(Enum value, ConstraintValidatorContext context) {
+        boolean result = false;
+        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        if (enumValues != null) {
+            for (Object enumValue : enumValues) {
+                if (value == enumValue) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
